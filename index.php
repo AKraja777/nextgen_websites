b58d7fc5e420d666cc2a942f0b3712ccdbef4c9c
AKraja777 <ashokkumar7hitter@gmail.com>
2024-01-27 14:10:54 +0530

    style.css

--- index.php
+++ index.php
@@ -83,7 +83,7 @@ if (isset($_POST['btnAdd'])) {
             </div>
             <div class="col-md-6" id="home-box" data-aos="fade-up">
                 <h1 class="text-start">BECOME FINANCIALLY INDEPENDENT</h1>
-                <p class="text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, maiores enim soluta dolorum quam, sapiente voluptates vel, minima libero sint ducimus commodi cupiditate blanditiis molestias! Animi voluptate veritatis pariatur quisquam! Temporibus, maiores enim soluta dolorum quam</p>
+                <p class="text-start">Welcome to NextGen Company, where we pave the way for your retail career through our comprehensive 7-day career-building course. Embrace the future of work with our retail order fulfillment training, unlocking exciting opportunities for online jobs that you can conveniently do from the comfort of your home. Join us in shaping the next generation of retail professionals and discover a path to success with NextGen Company</p>
             </div>
         </div>
     </div>
@@ -96,19 +96,19 @@ if (isset($_POST['btnAdd'])) {
                 <div class="col-md-4">
                     <div class="home-box1" data-aos="fade-up">
                         <h1 class="text-start" >7 Days Retail Career Building Course - Online course</h1>
-                        <p class="text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <p class="text-start">Join The Course In Any Language That You Prefer.</p>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="home-box2" data-aos="fade-up">
                         <h1 class="text-start">Get Certificate On Completation Of Course</h1>
-                        <p class="text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <p class="text-start">Get Job Opportunity In Retail Stores Or With NextGen Order Fulfilment.</p>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="home-box3" data-aos="fade-up">
                         <h1 class="text-start">Get Perment Work From Home Online Job</h1>
-                        <p class="text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <p class="text-start">Get Increments & Promotions In The Job.</p>
                     </div>
                 </div>
             </div>
@@ -127,7 +127,7 @@ if (isset($_POST['btnAdd'])) {
                           </svg>
                         </div>
                         <h1 class="text-start">10,403</h1>
-                        <p class="text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <p class="text-start">Registered With Retails Stores.</p>
                     </div>
                   
                     <div class="home-box5" data-aos="fade-up">
@@ -136,8 +136,8 @@ if (isset($_POST['btnAdd'])) {
                                 <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4"/>
                               </svg>
                         </div>
-                        <h1 class="text-start" >10,403</h1>
-                        <p class="text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <h1 class="text-start" >28000+</h1>
+                        <p class="text-start">Employment Opportunities.</p>
                     </div>
                     <div class="home-box6" data-aos="fade-up">
                         <div class="icon2">
@@ -145,8 +145,8 @@ if (isset($_POST['btnAdd'])) {
                                 <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.73 1.73 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.73 1.73 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.73 1.73 0 0 0 3.407 2.31zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z"/>
                               </svg>
                         </div>
-                        <h1 class="text-start" >10,403</h1>
-                        <p class="text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <h1 class="text-start" >1680</h1>
+                        <p class="text-start">Employees Hired.</p>
                     </div>
                 </div>
               
@@ -193,7 +193,7 @@ if (isset($_POST['btnAdd'])) {
                     </div>
                     
                     <div class="home-box11" data-aos="fade-up">
-                        <p class="text-start">What Nextgen Does For Retail Stores..</p>
+                        <p class="text-start">Job Details With NextGen & Earning Opportunities</p>
                     </div>
                     
                     <div class="col-md-6 mt-3" data-aos="fade-up">
@@ -210,7 +210,7 @@ if (isset($_POST['btnAdd'])) {
                     <div class="d-flex flex-column align-items-end">
                     
                         <h1 class="my-5" data-aos="fade-up" style="color:black; font-size: 2.5em;">Want to take a trial of jobs before enrolling in the course??</h1>
-                        <button class="btn btn-secondary mt-3" data-aos="fade-up" id="button" style="font-size: 1.5em; border-radius: 10px;font-weight: bold; background: #38b6ff;color:blue;">
+                        <button  onclick="window.location.href='https://next-gen-a052d.web.app/'"  class="btn btn-secondary mt-3" data-aos="fade-up" id="button" style="font-size: 1.5em; border-radius: 10px;font-weight: bold; background: #38b6ff;color:blue;">
                             Click Here</i>
                             <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="white" class="bi bi-arrow-right" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
@@ -225,6 +225,25 @@ if (isset($_POST['btnAdd'])) {
                 </div>
             </div>
         </div>
+    </section>
+    <section id="contact" class="bg-primary text-white">
+    <div class="container box">
+        <div class="row">
+        <h1 class="my-5" style="color:black; font-weight:bold;" data-aos="fade-up">Ready to work with us??</h1>
+                <div class="col-lg-6 col-md-6 col-12">
+                  <img src="img/img2.png" data-aos="fade-up"  class="img-fluid">
+                </div>
+    <div class="col-lg-6 col-md-6 col-12">
+        <h1 style="color:black; font-size: 2.5em;" data-aos="fade-up">APPLY NOW</h1>
+        <form method="post" href="index.php" enctype="multipart/form-data" data-aos="fade-up">
+    <input type="text"  class="form-control" id="name" name="name" placeholder="enter your name" required>
+    <input type="mail"  class="form-control" id="email" name="email" placeholder="enter your mail" name="email" required>
+    <input type="number"  class="form-control" id="mobile" name="mobile" placeholder="enter your Contact Number" name="mobile" required>
+    <input type="text"  class="form-control" id="location" name="location" placeholder="enter your Location" name="location" required>
+    <button type="submit" class="btn signin" name="btnAdd">Enroller Now</button>
+</form>
+        </div>
+   </div>
     </section>
     <section id="ready" class="bg-primary text-white">
         <div class="container-5">
@@ -253,25 +272,6 @@ if (isset($_POST['btnAdd'])) {
         </div>
     </div>
 </section>
-    <section id="contact" class="bg-primary text-white">
-    <div class="container box">
-        <div class="row">
-        <h1 class="my-5" style="color:black; font-weight:bold;" data-aos="fade-up">Ready to work with us??</h1>
-                <div class="col-lg-6 col-md-6 col-12">
-                  <img src="img/img2.png" data-aos="fade-up"  class="img-fluid">
-                </div>
-    <div class="col-lg-6 col-md-6 col-12">
-        <h1 style="color:black; font-size: 2.5em;" data-aos="fade-up">APPLY NOW</h1>
-        <form method="post" href="index.php" enctype="multipart/form-data" data-aos="fade-up">
-    <input type="text"  class="form-control" id="name" name="name" placeholder="enter your name" required>
-    <input type="mail"  class="form-control" id="email" name="email" placeholder="enter your mail" name="email" required>
-    <input type="number"  class="form-control" id="mobile" name="mobile" placeholder="enter your Contact Number" name="mobile" required>
-    <input type="text"  class="form-control" id="location" name="location" placeholder="enter your Location" name="location" required>
-    <button type="submit" class="btn signin" name="btnAdd">Enroller Now</button>
-</form>
-        </div>
-   </div>
-    </section>
 
 <section id="footer" class="bg-primary text-white">
         <div class="container-5">
